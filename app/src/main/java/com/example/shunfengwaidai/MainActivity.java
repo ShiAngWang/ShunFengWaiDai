ad13262607f60404b1d750f5e128261e71aefa28
liwensheng <1500259680@qq.com>
2018-04-09 18:31:48 +0800

    轮播图

--- app/src/main/java/com/example/shunfengwaidai/MainActivity.java
+++ app/src/main/java/com/example/shunfengwaidai/MainActivity.java
@@ -1,13 +1,81 @@
 package com.example.shunfengwaidai;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.bigkoo.convenientbanner.ConvenientBanner;
+import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
+import com.bigkoo.convenientbanner.holder.Holder;
+import com.bigkoo.convenientbanner.listener.OnItemClickListener;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements OnItemClickListener {
+
+    private ConvenientBanner convenientBanner;
+    private ArrayList<Integer> localImages = new ArrayList<Integer>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        convenientBanner=findViewById(R.id.convenientBanner);
+        for (int position = 1; position < 6; position++) {
+            localImages.add(getResId("ic_test_" + position, R.drawable.class));
+        }
+        convenientBanner.setPages(new CBViewHolderCreator() {
+            @Override
+            public Object createHolder() {
+                return new LocalImageHolderView();
+            }
+        },localImages)
+                .setPointViewVisible(true)
+                //设置自动切换（同时设置了切换时间间隔）
+                .startTurning(2000)
+                .setPageIndicator(new int[]{R.drawable.ic_page_indicator, R.drawable.ic_page_indicator_focused})
+                .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.ALIGN_PARENT_RIGHT)
+                //设置点击监听事件
+                .setOnItemClickListener(this)
+                //设置手动影响（设置了该项无法手动切换）
+                .setManualPageable(true);
+
+
+
     }
+    @Override
+    public void onItemClick(int position) {
+        Toast.makeText(this, "position:" + position, Toast.LENGTH_SHORT).show();
+    }
+    private class LocalImageHolderView implements Holder<Integer> {
+        private ImageView imageView;
+
+        @Override
+        public View createView(Context context) {
+            //你可以通过layout文件来创建，不一定是Image，任何控件都可以进行翻页
+            imageView = new ImageView(context);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            return imageView;
+        }
+
+        @Override
+        public void UpdateUI(Context context, int position, Integer data) {
+            imageView.setImageResource(data);
+        }
+
+    }
+
+    public static int getResId(String variableName, Class<?> c) {
+        try {
+            Field idField = c.getDeclaredField(variableName);
+            return idField.getInt(idField);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
 }
